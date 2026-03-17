b8f8585a5054784aee139855c083c4c7cf3653bd
i-amDev <dev79037@gmail.com>
2026-03-17 12:10:58 +0530

    - Learned about pipe method in streams

--- streams.js
+++ streams.js
@@ -3,25 +3,31 @@ const server = require('http').createServer();
 
 server.on('request', (request, response) => {
     // Solution - 1
-    fs.readFile('test-file.txt', (error, data) => {
-        if (error) {
-            console.log(error);
-        }
-        response.end(data);
-    });
+    // fs.readFile('test-file.txt', (error, data) => {
+    //     if (error) {
+    //         console.log(error);
+    //     }
+    //     response.end(data);
+    // });
+
     // Solution - 2 : Streams
+    // const readable = fs.createReadStream('test-file.txt');
+    // readable.on('data', chunk => {
+    //     response.write(chunk);
+    // })
+    // readable.on('end', () => {
+    //     response.end();
+    // })
+    // readable.on('error', error => {
+    //     console.log(error);
+    //     response.statusCode = 500;
+    //     response.end('File not found');
+    // })
+
+    // Solution - 3
     const readable = fs.createReadStream('test-file.txt');
-    readable.on('data', chunk => {
-        response.write(chunk);
-    })
-    readable.on('end', () => {
-        response.end();
-    })
-    readable.on('error', error => {
-        console.log(error);
-        response.statusCode = 500;
-        response.end('File not found');
-    })
+    readable.pipe(response);
+
 
 });
 
