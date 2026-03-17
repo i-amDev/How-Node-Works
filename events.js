fed30e2f9efbe0287f98b8d70c7d2748b063976b
i-amDev <dev79037@gmail.com>
2026-03-17 11:38:21 +0530

    - Created a server to learn about events on server

--- events.js
+++ events.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const http = require('http');
 
 class Sales extends EventEmitter {
     constructor() {
@@ -21,3 +22,22 @@ myEmitter.on('newSale', stock => {
 });
 
 myEmitter.emit('newSale', 10);
+
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+    console.log('Request received!');
+    response.end('Request received');
+});
+
+server.on('request', (request, response) => {
+    console.log('Another request received!😃');
+});
+
+server.on('close', () => {
+    console.log('Server closed!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Waiting for requests...')
+})
